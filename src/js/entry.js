226e6796d8f26234cb0b82493b283ad1e7e56816
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-01-03 11:32:47 +0000

    Tweaks to layout

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,26 +1,20 @@
 const features = [
-  [
-    "animate",
-    document.querySelector(":root"),
-    "Web Animation API",
-    "https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API"
-  ],
-  [
-    "registerProperty",
-    CSS,
-    "CSS Properties & Values API",
-    "https://drafts.css-houdini.org/css-properties-values-api/"
-  ],
-  [
-    "CSSUnitValue",
-    window,
-    "Typed OM",
-    "https://github.com/w3c/css-houdini-drafts/tree/master/css-typed-om"
-  ]
-].reduce((acc, [prop, obj, label, url]) => {
-  const feature = { label, url, enabled: prop in obj };
-  return [...acc, feature];
-}, []);
+  {
+    enabled: "animate" in document.querySelector(":root"),
+    label: "Web Animation API",
+    url: "https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API"
+  },
+  {
+    enabled: "registerProperty" in CSS,
+    label: "CSS Properties & Values API",
+    url: "https://drafts.css-houdini.org/css-properties-values-api/"
+  },
+  {
+    enabled: "CSSUnitValue" in window,
+    label: "Typed OM",
+    url: "https://github.com/w3c/css-houdini-drafts/tree/master/css-typed-om"
+  }
+];
 
 if (features.every(f => f.enabled)) {
   import("./jeffsum.js").then(module => module.showJeff());
---
+++ src/js/jeffsum.js
@@ -0,0 +1,53 @@
+CSS.registerProperty({
+  name: "--gradientStart",
+  syntax: "<color>",
+  initialValue: "#61BFD9",
+  inherits: true
+});
+CSS.registerProperty({
+  name: "--gradientEnd",
+  syntax: "<color>",
+  initialValue: "#0551B4",
+  inherits: true
+});
+
+const root = document.querySelector(":root");
+const hero = document.querySelector(".app__header");
+const jeffs = [...hero.querySelectorAll("img")];
+
+const config = {
+  duration: 3000,
+  fill: "both",
+  easing: "ease-in-out"
+};
+
+const switchJeff = (currentJeff, nextIndex) => {
+  jeffs.forEach(j => (j.style.zIndex = 0));
+  currentJeff.style.zIndex = 1;
+  showJeff(nextIndex);
+};
+
+export const showJeff = (currentIndex = 0) => {
+  const jeff = jeffs[currentIndex];
+  const { gradientStart, gradientEnd } = jeff.dataset;
+
+  root.style.setProperty("--gradientStart", gradientStart);
+  root.style.setProperty("--gradientEnd", gradientEnd);
+
+  jeff.style.zIndex = 2;
+  jeff.animate(
+    [
+      { opacity: 0, transform: "scale(1.2, 1.2)" },
+      { opacity: 1, transform: "scale(1, 1)" }
+    ],
+    config
+  ).onfinish = () => {
+    const nextIndex = currentIndex + 1;
+    setTimeout(switchJeff, config.duration, jeff, nextIndex % 3);
+  };
+};
+
+root.addEventListener("submit", e => {
+  e.preventDefault();
+  window.location.hash = "acknowledgements";
+});
