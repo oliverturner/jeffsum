6412aeadc5bb8f67abbb4ae7302e38c640957e4f
Oliver Turner <oliverturner@users.noreply.github.com>
2023-03-05 19:10:14 +0000

    Generate paragraphs

--- src/js/jeffsum.ts
+++ src/js/jeffsum.ts
@@ -4,46 +4,47 @@ const els = {
   root: document.querySelector<HTMLHtmlElement>(":root")!,
   hero: document.querySelector<HTMLElement>(".app__header")!,
   form: document.querySelector<HTMLFormElement>("form[name='jeffness']")!,
-  quotes: document.querySelector<HTMLDivElement>(
-    "[data-component='quotes']"
-  )!
+  quotes: document.querySelector<HTMLDivElement>("[data-component='quotes']")!,
 };
 
 const jeffs = els.hero.querySelectorAll<HTMLImageElement>("img");
 const jeffNum = jeffs.length;
 
-const config = {
+const ANIM_PROPS = [
+  { opacity: 0, transform: "scale(1.2, 1.2)" },
+  { opacity: 1, transform: "scale(1, 1)" },
+];
+const ANIM_CONFIG = {
   duration: 3000,
   fill: "both",
   easing: "ease-in-out",
 } as const;
 
-function switchJeff(currentJeff: HTMLImageElement, nextIndex: number) {
+function switchJeff(props: {
+  currentJeff: HTMLImageElement;
+  nextIndex: number;
+}) {
   for (const jeff of jeffs) {
     jeff.style.setProperty("z-index", "0");
   }
 
-  currentJeff.style.setProperty("z-index", "1");
-  showJeff(nextIndex);
+  props.currentJeff.style.setProperty("z-index", "1");
+  showJeff(props.nextIndex);
 }
 
-export function showJeff(currentIndex = 0) {
-  const jeff = jeffs[currentIndex];
-  const { gradientStart, gradientEnd } = jeff.dataset;
+export function showJeff(index = 0) {
+  const currentJeff = jeffs[index % jeffNum];
+  const { gradientStart, gradientEnd } = currentJeff.dataset;
 
   els.root.style.setProperty("--gradientStart", gradientStart!);
   els.root.style.setProperty("--gradientEnd", gradientEnd!);
 
-  jeff.style.setProperty("z-index", "2");
-  jeff.animate(
-    [
-      { opacity: 0, transform: "scale(1.2, 1.2)" },
-      { opacity: 1, transform: "scale(1, 1)" },
-    ],
-    config
-  ).onfinish = () => {
-    const nextIndex = currentIndex + 1;
-    setTimeout(switchJeff, config.duration, jeff, nextIndex % jeffNum);
+  currentJeff.style.setProperty("z-index", "2");
+  currentJeff.animate(ANIM_PROPS, ANIM_CONFIG).onfinish = () => {
+    setTimeout(switchJeff, ANIM_CONFIG.duration, {
+      currentJeff,
+      nextIndex: index + 1,
+    });
   };
 }
 
@@ -56,11 +57,23 @@ function shuffle(a: string[]) {
   return a;
 }
 
+function getParas(num: number) {
+  const paras: string[] = [];
+  const numSentences = Math.floor(Math.random() * 5) + 2;
+
+  for (let i = 0; i < num; i++) {
+    const str = shuffle(quotes).slice(0, numSentences).join(" ");
+    paras.push(`<p>${str}</p>`);
+  }
+
+  return paras;
+}
+
 els.form.addEventListener("submit", function (event: SubmitEvent) {
   event.preventDefault();
 
   const f = new FormData(this);
-  const n = f.get("paragraphs");
+  const n = parseInt(String(f.get("paragraphs")) || "5", 10);
 
-  console.log(this, n);
+  els.quotes.innerHTML = getParas(n).join("");
 });
